4a971ae17749e85c5196761d68fe188fd64b17da
mattscepter <chaudharyut25@gmail.com>
2021-10-30 17:04:23 +0530

    new

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 const { default: axios } = require('axios');
 
 const axiosInstance = axios.create({
-  baseURL: 'http://ec2-13-234-29-23.ap-south-1.compute.amazonaws.com:6000/api',
+  baseURL: 'https://furniture-crm-backend.herokuapp.com/api',
 });
 
 export default axiosInstance;
