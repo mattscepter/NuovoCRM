987cd7a3f384bb09d72b508d4ad9c37b347ea1bb
mattscepter <chaudharyut25@gmail.com>
2021-10-30 16:56:12 +0530

    new

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 const { default: axios } = require('axios');
 
 const axiosInstance = axios.create({
-  baseURL: 'http://ec2-13-234-29-23.ap-south-1.compute.amazonaws.com/api',
+  baseURL: 'http://ec2-13-234-29-23.ap-south-1.compute.amazonaws.com:6000/api',
 });
 
 export default axiosInstance;
