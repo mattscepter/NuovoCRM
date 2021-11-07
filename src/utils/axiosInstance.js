c191a47db82bde544787d5fae6960623109cb6d6
HarJeet <43493956+harry-dev98@users.noreply.github.com>
2021-11-07 21:18:16 +0530

    REACT_APP_ added as prefix to env var

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 const { default: axios } = require('axios');
 
 const axiosInstance = axios.create({
-  baseURL: process.env.API_URL || 'https://furniture-crm-backend.herokuapp.com/api',
+  baseURL: process.env.REACT_APP_API_URL || 'https://furniture-crm-backend.herokuapp.com/api',
 });
 
 export default axiosInstance;
