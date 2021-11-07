6af6e489a2a881f0ea7aecc974db8957dbb279c3
HarJeet <43493956+harry-dev98@users.noreply.github.com>
2021-11-07 22:06:07 +0530

    base url to new deployment on aws

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 const { default: axios } = require('axios');
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://furniture-crm-backend.herokuapp.com/api',
+  baseURL: process.env.REACT_APP_API_URL || 'http://3.109.153.114/api',
 });
 
 export default axiosInstance;
