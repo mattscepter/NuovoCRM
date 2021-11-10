7ccce0e95a3fda0b948ba1df82f690e00a610d48
HarJeet <43493956+harry-dev98@users.noreply.github.com>
2021-11-10 18:13:02 +0530

    nuovoindia.link

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 const { default: axios } = require('axios');
 
 const axiosInstance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://furniture-crm-backend.herokuapp.com/api',
+  baseURL: process.env.REACT_APP_API_URL || 'https://api.nuovoindia.link/api',
 });
 
 export default axiosInstance;
